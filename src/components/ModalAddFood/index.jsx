84490610d0bc271eea9b8ccecda08ed8ca580697
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-06-28 13:26:29 -0300

    refactor: ModalAddFood e Modal

--- src/components/ModalAddFood/index.jsx
+++ src/components/ModalAddFood/index.jsx
@@ -1,47 +1,55 @@
-import { Component, createRef } from 'react';
+import { Component, createRef, useState } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
-import Modal from '../Modal';
+import { Modal }  from '../Modal';
 import Input from '../Input';
 
-class ModalAddFood extends Component {
-  constructor(props) {
-    super(props);
+export function ModalAddFood( { isOpen, setIsOpen, handleAddFood }) {
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [price, setPrice] = useState(0)
+  const [image, setImage] = useState('')
+  
+  async function handleSubmit () {
 
-    this.formRef = createRef();
-  }
-
-  handleSubmit = async data => {
-    const { setIsOpen, handleAddFood } = this.props;
-
-    handleAddFood(data);
+    handleAddFood({ name, description, price, image });
     setIsOpen();
-  };
-
-  render() {
-    const { isOpen, setIsOpen } = this.props;
-
-    return (
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit}>
-          <h1>Novo Prato</h1>
-          <Input name="image" placeholder="Cole o link aqui" />
-
-          <Input name="name" placeholder="Ex: Moda Italiana" />
-          <Input name="price" placeholder="Ex: 19.90" />
-
-          <Input name="description" placeholder="Descrição" />
-          <button type="submit" data-testid="add-food-button">
-            <p className="text">Adicionar Prato</p>
-            <div className="icon">
-              <FiCheckSquare size={24} />
-            </div>
-          </button>
-        </Form>
-      </Modal>
-    );
   }
-};
+   
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input 
+          name="image" 
+          placeholder="Cole o link aqui" 
+          onChange={event => setImage(event.target.value)}
+        />
+        <Input 
+          name="name" 
+          placeholder="Ex: Moda Italiana" 
+          onChange={event => setName(event.target.value)}
+        />
+        <Input 
+          name="price" 
+          placeholder="Ex: 19.90" 
+          onChange={event => setPrice(event.target.value)}
+        />
 
-export default ModalAddFood;
+        <Input 
+          name="description" 
+          placeholder="Descrição" 
+          onChange={event => setDescription(event.target.value)}
+        />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
+
